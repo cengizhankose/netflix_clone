59e3df4abfa40b90d339758b371c8cfd7e32dec2
Cengizhan KÖSE <kose651@gmail.com>
2020-08-06 00:52:08 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import {SafeAreaView, ScrollView, View, Image, Text} from 'react-native';
-import {Button, Input} from './src/components';
+import {Button, Input, CheckBox} from './src/components';
 
 export default class App extends Component {
+  state = {
+    isRememberMe: false,
+  };
+
   render() {
+    const {isRememberMe} = this.state;
     return (
       <SafeAreaView
         style={{
@@ -21,25 +26,41 @@ export default class App extends Component {
               source={require('./src/images/logo.png')}
             />
           </View>
-          <View style={{flex: 2, backgroundColor: 'black', paddingLeft: '10%'}}>
+          <View
+            style={{
+              flex: 2.5,
+              backgroundColor: 'black',
+              paddingLeft: '10%',
+              justifyContent: 'center',
+            }}>
             <Text
               style={{
                 color: 'white',
                 fontWeight: 'bold',
                 fontSize: 32,
-                marginTop:'5%'
               }}>
               Sign In
             </Text>
-            <Input placeholder={'Email or phone number'} style={{marginTop:'7%'}} />
-            <Input placeholder={'Password'} style={{marginTop:'4%'}}/>
+            <Input
+              placeholder={'Email or phone number'}
+              style={{marginTop: '7%'}}
+            />
+            <Input placeholder={'Password'} style={{marginTop: '4%'}} />
             <Button
               text={'Sign In'}
-              style={{marginTop:'10%'}}
+              style={{marginTop: '10%'}}
               onPress={() => {
                 console.log('tusa basildi');
               }}
             />
+            <View style={{flexDirection: 'row',justifyContent:"space-between",width:'90%',alignItems:"flex-end"}}>
+              <CheckBox
+                text="Remember Me"
+                status={isRememberMe}
+                onPress={() => this.setState({isRememberMe: !isRememberMe})}
+              />
+              <Text style={{color:'gray'}}>Need help?</Text>
+            </View>
           </View>
           <View
             style={{
